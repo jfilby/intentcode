616a363dd041352bfcdc291e180e1393f846e48a
jfilby <jason.filby@gmail.com>
2025-12-29 12:02:21 +0200

    Added code to get/create dir/filename nodes for an IntentCode file.

---
+++ src/nextjs/server/src/services/graph/path-graph-mutate-service.ts
@@ -0,0 +1,67 @@
+import { PrismaClient, SourceNode } from '@prisma/client'
+import { CustomError } from '@/serene-core-server/types/errors'
+import { FsUtilsService } from '../intentcode/utils/fs-utils-service'
+import { GraphMutateService } from './graph-mutate-service'
+
+// Services
+const fsUtilsService = new FsUtilsService()
+const graphMutateService = new GraphMutateService()
+
+// Class
+export class PathGraphMutateService {
+
+  // Consts
+  clName = 'PathGraphMutateService'
+
+  // Code
+  async getOrCreatePathAsGraph(
+          prisma: PrismaClient,
+          projectSourceNode: SourceNode,
+          fullPath: string) {
+
+    // Debug
+    const fnName = `${this.clName}.getOrCreatePathAsGraph()`
+
+    // Validate project path
+    if (projectSourceNode.path == null ||
+        !fullPath.startsWith(projectSourceNode.path)) {
+
+      throw new CustomError(
+        `${fnName}: Invalid path: ${fullPath} for project source node: ` +
+        `${projectSourceNode.path}`)
+    }
+
+    // Strip project path from fullPath prefix
+    // The fullPath must have been verified as starting with the project path
+    const relativePath = fullPath.slice(projectSourceNode.path.length)
+
+    // Split up dirs and filename
+    const filename = fsUtilsService.getFilenamePart(relativePath)
+    const dirsPath = fsUtilsService.getDirectoriesPart(relativePath)
+    const dirs = fsUtilsService.getDirectoriesArray(dirsPath)
+
+    // Get/create nodes for dirs
+    var dirSourceNode: SourceNode = projectSourceNode
+
+    for (const dir of dirs) {
+
+      dirSourceNode = await
+        graphMutateService.getOrCreateIntentCodeDir(
+          prisma,
+          projectSourceNode.instanceId,
+          dirSourceNode,
+          dir)
+    }
+
+    // Get/create nodes for the filename
+    const filenameSourceNode = await
+            graphMutateService.getOrCreateIntentCodeFile(
+              prisma,
+              projectSourceNode.instanceId,
+              dirSourceNode,
+              filename)
+
+    // Return filename's node
+    return filenameSourceNode
+  }
+}
---
+++ src/nextjs/server/src/services/intentcode/utils/fs-utils-service.ts
@@ -0,0 +1,101 @@
+import path from 'path'
+import { CustomError } from '@/serene-core-server/types/errors'
+
+export class FsUtilsService {
+
+  // Consts
+  clName = 'FsUtilsService'
+
+  // Code
+  concatPaths(
+    part1: string,
+    part2: string) {
+
+    if (part1[part1.length - 1] !== path.sep) {
+
+      part1 += path.sep
+    }
+
+    return part1 + part2
+  }
+
+  getDirectoriesPart(fullPath: string) {
+
+    return path.dirname(fullPath)
+  }
+
+  getDirectoriesArray(dirsPath: string) {
+
+    return dirsPath.split(path.sep)
+  }
+
+  getFileExtension(filenamePath: string) {
+
+    // Debug
+    const fnName = `${this.clName}.getFileExtension()`
+
+    // Validate
+    if (filenamePath == null) {
+
+      throw new CustomError(`${fnName}: filenamePath == null`)
+    }
+
+    // Get the file extension
+    var fileExt = path.extname(filenamePath)
+
+    if (fileExt.length > 0 &&
+        fileExt[0] === '.') {
+
+      fileExt = fileExt.substring(1)
+    }
+
+    // Return
+    return fileExt
+  }
+
+  getFilenamePart(fullPath: string) {
+
+    return path.basename(fullPath)
+  }
+
+  getLastPathPart(fullPath: string) {
+
+    const lastPathPart = fullPath.split(path.sep).pop()
+
+    return lastPathPart
+  }
+
+  getNameAndFileExtensionParts(filenamePath: string) {
+
+    const fileExtensionPart = this.getFileExtension(filenamePath)
+
+    var extLength = fileExtensionPart.length
+
+    if (fileExtensionPart !== '') {
+      extLength += 1
+    }
+
+    const namePart = filenamePath.substring(0, filenamePath.length - extLength)
+
+    return {
+      fileExtensionPart: fileExtensionPart,
+      namePart: namePart
+    }
+  }
+
+  getRelativePath(
+    fullPath: string,
+    basePath: string) {
+
+    // Remove the base path
+    var relativePath =
+          fullPath.replace(
+          basePath, '')
+
+    // Note: don't removing the leading path separator. The convention is that
+    // all instance paths have a leading separator.
+
+    // Return
+    return relativePath
+  }
+}
