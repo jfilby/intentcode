3c46fe390a317db36f26a9f2b752b3c1275c0507
jfilby <jason.filby@gmail.com>
2025-12-30 14:46:16 +0200

    Re-org TypeScript-specific prompting.

--- src/nextjs/server/src/services/intentcode/compiler/code/target-lang-service.ts
+++ src/nextjs/server/src/services/intentcode/compiler/code/target-lang-service.ts
@@ -22,15 +22,19 @@ export class TargetLangService {
 
   getTypeScriptPrompting() {
 
-    const prompt =
+    // General instructions
+    var prompt =
       `- Wrap functions and basic static types in a class where possible. ` +
       `  Use the H1 name as the class name.\n` +
-      `- Instantiate classes that aren't static before using them.\n` +
-
-      // removed, now test without: `in the AST tree`
       `- Track these attributes where relevant:\n `+
       `  async, export, generator, static.\n`
 
+    // Prompting to coerce the compiler LLM to produce correct code
+    prompt +=
+      `- Instantiate classes before using them to call non-static ` +
+      `  functions.\n`
+
+    // Return
     return prompt
   }
 }
