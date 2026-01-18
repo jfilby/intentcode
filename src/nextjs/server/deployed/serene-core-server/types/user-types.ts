2b65d152dc693d161fbcdd43449756526a1112ce
jfilby <jason.filby@gmail.com>
2026-01-18 16:11:56 +0200

    Initial server project.

---
+++ src/nextjs/server/deployed/serene-core-server/types/user-types.ts
@@ -0,0 +1,18 @@
+export class SereneCoreServerTypes {
+
+  // Statuses
+  static activeStatus = 'A'
+
+  // Names
+  static credits = 'Credits'
+
+  // Paid/free pricing tiers
+  static free = 'free'
+  static paid = 'paid'
+}
+
+export class UserTypes {
+
+  static botRoleOwnerType = 'B'
+  static humanRoleOwnerType = 'H'
+}
