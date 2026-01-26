281f455d1cc4efa04aff73d25176e76bf57b2339
jfilby <jason.filby@gmail.com>
2026-01-26 08:04:50 +0200

    Include IntentCode definition prompting.

--- src/nextjs/engine/src/services/intentcode/common/types.ts
+++ src/nextjs/engine/src/services/intentcode/common/types.ts
@@ -3,5 +3,6 @@ export class IntentCodeCommonTypes {
   static intentCodePrompting =
     `IntentCode:\n` +
     `- Is pseudo-code, don't interpret it as literal code.\n` +
-    `- Uses Markdown format to describe the intent.\n`
+    `- Has a focus on intent and doesn't include programming details.\n` +
+    `- Uses Markdown format to reflect high-level code structure.\n`
 }
